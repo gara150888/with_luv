e77b52e0633357cc0281971b6bf733f6c1139c53
gara150888 <devgogogogogo@gmail.com>
2026-05-05 21:02:20 +0530

    theme setup

---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { AppSidebar } from "@/components/layout/app-sidebar"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+import { usePathname } from "next/navigation"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+
+    const path = usePathname();
+
+
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Breadcrumb>
+                            <BreadcrumbList>
+                                <BreadcrumbItem className="hidden md:block">
+                                    <BreadcrumbLink href="#">
+                                        Tournament4All
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator className="hidden md:block" />
+                                <BreadcrumbItem>
+                                    <BreadcrumbPage className="capitalize">
+                                        {path.split('/')[1] || "Unkown Page"}
+                                    </BreadcrumbPage>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                </header>
+                <div className="flex flex-1">
+                    {children}
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+    )
+}
